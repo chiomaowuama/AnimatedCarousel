cf18bdf3f3a51092507173f90e59e16b1c23531d
chiomaowuama <108200300+chiomaowuama@users.noreply.github.com>
2024-01-23 19:39:30 +0100

    the first

---
+++ src/views/HomeView.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+</script>
+
+<template>
+  <main>
+
+    <div class="drag-container">
+      <div class="spin-container">
+        <!-- images -->
+        <img src="https://images.unsplash.com/photo-1440589473619-3cde28941638?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y29vcnBlcmF0ZSUyMHBlb3BsZXxlbnwwfHwwfHx8MA%3D%3D">
+        <img src="https://images.unsplash.com/photo-1534528741775-53994a69daeb?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y29vcnBlcmF0ZSUyMHBlb3BsZXxlbnwwfHwwfHx8MA%3D%3D">
+        <img src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8Y29vcnBlcmF0ZSUyMHBlb3BsZXxlbnwwfHwwfHx8MA%3D%3D">
+        <img src="https://images.unsplash.com/photo-1489424731084-a5d8b219a5bb?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fGNvb3JwZXJhdGUlMjBwZW9wbGV8ZW58MHx8MHx8fDA%3D">
+        <img src="https://images.unsplash.com/photo-1488426862026-3ee34a7d66df?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fGNvb3JwZXJhdGUlMjBwZW9wbGV8ZW58MHx8MHx8fDA%3D">
+        <img src="https://images.unsplash.com/photo-1526413232644-8a40f03cc03b?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGNvb3JwZXJhdGUlMjBwZW9wbGV8ZW58MHx8MHx8fDA%3D">
+        <!-- video here -->
+        <video controls autoplay="autoplay" loop>
+          <source src="https://player.vimeo.com/external/479728625.sd.mp4?s=f4f886d3d45a0312d8d47419647788178535a2c6&profile_id=165&oauth2_token_id=57447761" type="video/mp4">
+        </video>
+        <!-- text at the center of the ground -->
+        <p>3D carousel</p>
+      </div>
+      <div id="ground"></div>
+    </div>
+  </main>
+</template>
