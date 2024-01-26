9e098abafdefadaf3561dfb867f58fae70070a8f
chiomaowuama <108200300+chiomaowuama@users.noreply.github.com>
2024-01-26 13:49:07 +0100

    the script

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <script setup>
-  import { onMounted, ref } from 'vue';
+  import { ref, onMounted } from 'vue';
 
   let raduis = ref(240);
   let autoRotate = ref(true);
@@ -7,16 +7,33 @@
   let imgwidth = ref(120)
   let imgheight = ref(170);
   let dragcontainer =ref('');
-  let spincontainer = ref('')
-  // setTimeout(init, 1000)
- 
+  let spincontainer = ref('');
+  let videoUrl = ref("https://player.vimeo.com/external/479728625.sd.mp4?s=f4f886d3d45a0312d8d47419647788178535a2c6&profile_id=165&oauth2_token_id=57447761")
+  let videosHolder = ref([]);
+  const images = ref([
+    "https://images.unsplash.com/photo-1440589473619-3cde28941638?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y29vcnBlcmF0ZSUyMHBlb3BsZXxlbnwwfHwwfHx8MA%3D%3D",
+    "https://images.unsplash.com/photo-1534528741775-53994a69daeb?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y29vcnBlcmF0ZSUyMHBlb3BsZXxlbnwwfHwwfHx8MA%3D%3D",
+    "https://images.unsplash.com/photo-1494790108377-be9c29b29330?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8Y29vcnBlcmF0ZSUyMHBlb3BsZXxlbnwwfHwwfHx8MA%3D%3D",
+    "https://images.unsplash.com/photo-1488426862026-3ee34a7d66df?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fGNvb3JwZXJhdGUlMjBwZW9wbGV8ZW58MHx8MHx8fDA%3D",
+    "https://images.unsplash.com/photo-1526413232644-8a40f03cc03b?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGNvb3JwZXJhdGUlMjBwZW9wbGV8ZW58MHx8MHx8fDA%3D",
+    " https://images.unsplash.com/photo-1488426862026-3ee34a7d66df?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fGNvb3JwZXJhdGUlMjBwZW9wbGV8ZW58MHx8MHx8fDA%3D",
+    // ... add other image URLs
+  ]);
+  // let alimg = ref([])
+  // let aEle =ref({image:alimg.value, video:videosHolder.value}) //this is the thing that isnt working 
+  // console.log(videosHolder)
+  // console.log(alimg.value)
+  // console.log(aEle.value)
 
   onMounted(() => {
-    setTimeout(init, 1000)
-        
+  //   let alimg = ref([])
+  // let aEle =ref({image:alimg.value, video:videosHolder.value}) //this is the thing that isnt working 
+  // console.log(videosHolder)
+  // console.log(alimg.value)
+  // console.log(aEle.value)
+    
     })
 </script>
-
 
 <template>
   <main>
@@ -24,15 +41,14 @@
     <div id="drag-container"  ref="dragcontainer">
       <div id="spin-container" ref="spincontainer">
         <!-- images -->
-        <img src="https://images.unsplash.com/photo-1440589473619-3cde28941638?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y29vcnBlcmF0ZSUyMHBlb3BsZXxlbnwwfHwwfHx8MA%3D%3D">
-        <img src="https://images.unsplash.com/photo-1534528741775-53994a69daeb?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y29vcnBlcmF0ZSUyMHBlb3BsZXxlbnwwfHwwfHx8MA%3D%3D">
-        <img src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8Y29vcnBlcmF0ZSUyMHBlb3BsZXxlbnwwfHwwfHx8MA%3D%3D">
-        <img src="https://images.unsplash.com/photo-1489424731084-a5d8b219a5bb?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fGNvb3JwZXJhdGUlMjBwZW9wbGV8ZW58MHx8MHx8fDA%3D">
-        <img src="https://images.unsplash.com/photo-1488426862026-3ee34a7d66df?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fGNvb3JwZXJhdGUlMjBwZW9wbGV8ZW58MHx8MHx8fDA%3D">
-        <img src="https://images.unsplash.com/photo-1526413232644-8a40f03cc03b?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGNvb3JwZXJhdGUlMjBwZW9wbGV8ZW58MHx8MHx8fDA%3D">
-        <!-- video here -->
-        <video controls autoplay="autoplay" loop>
-          <source src="https://player.vimeo.com/external/479728625.sd.mp4?s=f4f886d3d45a0312d8d47419647788178535a2c6&profile_id=165&oauth2_token_id=57447761" type="video/mp4">
+        <img  
+        v-for="(image, index) in images"
+        :key="index" alt="Unsplash Image"
+        :src="image" ref="alimg" >
+        
+        
+        <video controls autoplay="autoplay" loop ref="videosHolder">
+          <source :src="videoUrl" type="video/mp4">
         </video>
         <!-- text at the center of the ground -->
         <p>3D carousel</p>
